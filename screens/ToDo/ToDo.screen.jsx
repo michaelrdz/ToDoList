0305017d4e69c4a3529b5c2262e1755baa3c9756
Emmanuel Reyes <aerj941025@gmail.com>
2021-11-14 13:12:58 -0600

    Funciones de alta,baja y cambios en firebase

--- screens/ToDo/ToDo.screen.jsx
+++ screens/ToDo/ToDo.screen.jsx
@@ -1,20 +1,88 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
-//import { INFO_PAGE_STYLES as styles } from "./Info.styles";
-//import { TEXT_STYLES as textStyles } from "../../styles/text.styles";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, TextInput  } from "react-native";
+import { auth, database } from "../../firebase";
 
-import { auth } from "../../firebase";
   
 const ToDoScreen = ({}) => {
 
+  //const [usuario, setUsuario] = useState();
+  //const data = { "ID": "", "Email": "" };
+  const item = {"Titulo":"", "Estado":"Desactivar"};
+  const [titulo, setTitulo] = useState();
+
+  useEffect(() => {
+    // database
+    //   .ref()
+    //   .child("Usuarios")
+    //   .on(
+    //     "value",
+    //     (data) => {
+    //       const datas = data.val();
+    //       console.log(data);
+    //     },
+    //     []
+    //   );
+
+    listarItems();
+  });
+
+  // Nuevo Item
+  const crearItem = () => {
+    const todoRef = database.ref("Usuarios/"+auth.currentUser?.uid+"/Items");
+    item.Titulo = titulo;
+    todoRef.push(item);
+    console.log(item);
+  };
+  
+  // Consultar la informacion
+  const listarItems = () => {
+    const todoRef = database.ref("Usuarios/"+auth.currentUser?.uid+"/Items");
+    todoRef.on("value", (snapshot) => {
+      const items= snapshot.val();
+      const itemListar= [];
+      for (let id in items) {
+          itemListar.push({id, ...items[id] });
+      }
+      //setItemListar(itemListar);
+      console.log(itemListar);
+    } )
+  };
+
+    // Modificar Item
+    const modificarItem = ( ID, tit, estado ) => {
+      const todoRef = database.ref("Usuarios/"+usauth.currentUser?.uiduario+"/Items").child(ID);
+      todoRef.update({
+        Titulo: tit, Estado: estado,
+      });
+      listarItems();
+    };
+
+  // Eliminar Item
+  const eliminarItem = ( ID ) => {
+    const todoRef = database.ref("Usuarios/"+auth.currentUser?.uid+"/Items").child(ID);
+    todoRef.remove();
+    listarItems();
+  };
+
   return (
-    // Vista ToDo List
     <View style={styles.container}>
-        <Text>
-            Aquí va la lista de tareas (Agregar, cambiar estado, editar titulo, eliminar)
-        </Text>
-        <Text>Email:{auth.currentUser?.email}</Text>
-        <Text>User ID (UID):{auth.currentUser?.uid}</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setTitulo(text)}
+      />
+      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={() => {crearItem()}}
+        //onPress={() => { handleLogin(); listarItems()}}
+        //onPress={() => { handleLogin(); modificarItem("-MoEluHUTfaNhfhFoYsW", "SSSSSSS", "ACTIVAR")}}
+        //onPress={() => { handleLogin(); eliminarItem("-MoEniIk0lDPbJzhNjKs")}}
+        style={{ backgroundColor: "red", height:100, width:200, alignItems:"center", justifyContent: "center", }}
+      >
+        <Text>PROBAR</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
     </View>
   );
 };
@@ -23,7 +91,8 @@ export default ToDoScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
